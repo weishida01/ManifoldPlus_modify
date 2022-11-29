0ed126774fb1ed0236b87eb9afb54d734a575c7b
ShidaWei <65539711+weishida01@users.noreply.github.com>
2022-11-29 11:24:26 +0800

    Add files via upload

---
+++ build/CMakeFiles/manifold.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/code/paper/mesh_simplify/ManifoldPlus/src/IO.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/IO.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/Intersection.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/Intersection.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/Manifold.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/Manifold.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/MeshProjector.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/MeshProjector.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/Octree.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/Octree.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/Parser.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/Parser.cc.o"
+  "/code/paper/mesh_simplify/ManifoldPlus/src/main.cc" "/code/paper/mesh_simplify/ManifoldPlus/build/CMakeFiles/manifold.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../src"
+  "../3rd_party/eigen"
+  "../3rd_party/libigl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/manifold.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manifold.dir/src/Intersection.cc.o"
+  "CMakeFiles/manifold.dir/src/IO.cc.o"
+  "CMakeFiles/manifold.dir/src/main.cc.o"
+  "CMakeFiles/manifold.dir/src/Manifold.cc.o"
+  "CMakeFiles/manifold.dir/src/MeshProjector.cc.o"
+  "CMakeFiles/manifold.dir/src/Octree.cc.o"
+  "CMakeFiles/manifold.dir/src/Parser.cc.o"
+  "manifold.pdb"
+  "manifold"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manifold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
